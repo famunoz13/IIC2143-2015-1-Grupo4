b9f244dec3c19adc675c9fc919cea19190aeab82
Franco Muñoz <famunoz13@uc.cl>
2015-04-17 18:59:23 -0300

    Se creo que el proyecto en eclipse

---
+++ src/RestaurantApp.java
@@ -0,0 +1,14 @@
+//import javax.swing.*;
+import java.applet.*;
+import java.awt.*;
+//import java.awt.event.*;
+
+public class RestaurantApp extends Applet{
+
+	private static final long serialVersionUID = 1L;
+	private Label label1;
+	public void init(){
+		label1 = new Label("Hello World!");
+		add(label1);
+	}
+}

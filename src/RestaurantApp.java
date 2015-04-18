e32c01e409c913af56f35d0cf8a898feb88d9de4
Franco Muñoz <famunoz13@uc.cl>
2015-04-18 18:18:29 -0300

    Se añade la clase para la ventana principal

--- src/RestaurantApp.java
+++ src/RestaurantApp.java
@@ -1,14 +1,7 @@
-//import javax.swing.*;
-import java.applet.*;
-import java.awt.*;
-//import java.awt.event.*;
 
-public class RestaurantApp extends Applet{
-
-	private static final long serialVersionUID = 1L;
-	private Label label1;
-	public void init(){
-		label1 = new Label("Hello World!");
-		add(label1);
-	}
+public class RestaurantApp{
+  public static void main(String[] args) {
+    MainWindow main_window = new MainWindow();
+    main_window.setVisible(true);
+  }
 }
